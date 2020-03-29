94ca2114085ba138e3a4d260236a863b266595b6
Mikaio Melo <mikaiomelo23@gmail.com>
2020-03-29 10:45:09 -0300

    Added login page

---
+++ frontend/src/pages/Logon/index.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import { FiLogIn } from 'react-icons/fi';
+import { Link, useHistory } from 'react-router-dom'; // Using Link instead of <a>, the pge will not reload to the next
+
+import api from '../../services/api';
+
+import './styles.css';
+
+import logoImg from '../../assets/logo.svg';
+import heroesImg from '../../assets/heroes.png';
+
+export default function Logon() {
+    const [id, setId] = useState('');
+    const history = useHistory();
+
+    async function handleLogin(e){
+        e.preventDefault();
+
+        try {
+            const response = await api.post('session', { id });
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('/profile');
+        } catch (error) {
+            alert("ID incorreto!")
+        }
+    }
+
+    return (
+        <div className="logon-container">
+            <section className="form">
+                <img src={logoImg} alt="Be The Hero" />
+
+                <form onSubmit={handleLogin}>
+                    <h1>Faça seu logon</h1>
+                    <input 
+                     placeholder="Sua ID"
+                     value={id}
+                     onChange={e => setId(e.target.value)} />
+                    <button className="button" stype="submit">Entrar</button>
+
+                    <Link className="back-link" to="/register">
+                        <FiLogIn size={16} color="#e02041" />
+                        Não tenho cadastro
+                    </Link>
+                </form>
+            </section>
+            <img src={heroesImg} alt="Heroes" />
+        </div>
+    );
+}
