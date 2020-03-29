9defde10987b3dc3b0e8732315d0ffb2b4e0ece8
Mikaio Melo <mikaiomelo23@gmail.com>
2020-03-28 23:56:32 -0300

    Added routes file

---
+++ backend/src/routes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
+const SessionController = require('./controllers/SessionController')
+
+const routes = express.Router();
+
+// Session route
+routes.post('/session', SessionController.create);
+
+// ONGs routes
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+// Incidents routes
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
+routes.get('/profile', ProfileController.index);
+
+module.exports = routes;
